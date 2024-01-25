aff94a55f1e731e557cd86d24cd3e7a9f36345c8
Jermu Virtanen <jermuv@googlemail.com>
2024-01-25 14:14:16 +0200

    DUMPPID

---
+++ AD/1. Generate 1000 users/generate-csvfile.ps1
@@ -0,0 +1,11 @@
+# ideas for the file
+# ask the filenames for first names and last names
+## read the first names from the csv file
+## read the last names from the csv file
+# ask employeeid starting number
+# ask the structure of samaccountname
+## how many characters counted from the first name
+## how many characters counted from the last name
+## which is first part of the account? Which is the last part of the account
+# check the conflicts
+## if there is a conflict, generate a new row
